cb301dc4ec5dce7157b4e25289d4d59c7e5f20e0
Shaun Houck <shaun@Shauns-iMac.attlocal.net>
2022-02-16 18:40:06 -0700

    Charting working for high, low, and closing for last 30 days

--- apps.js
+++ apps.js
@@ -1,79 +1,132 @@
 const apiKey = "c86jhp2ad3iddpknn4s0";
-
-function getCurrentPrice(){
-    const form = document.getElementById('form')
-    form.addEventListener('submit', e => {
+const alphaApi = "16V2RBTG7QGIGOSS";
+function getCurrentPrice() {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let stock = document.getElementById('input').value.toUpperCase();
+    let stock = document.getElementById("input").value.toUpperCase();
     form.reset();
-    fetch(`https://finnhub.io/api/v1/quote?symbol=${stock}&token=${apiKey}`) 
-    .then(response => response.json())
-    .then(stockData => writeData(stockData, stock))
-    .then(stockData => getCandleData(stockData,stock));
-});
-} 
-
-function getCandleData(stockData,stock){
-    let previousMonth = Math.round(new Date(new Date().setDate(new Date().getDate()-30))/1000);
-    let currDay = Math.round((new Date()).getTime() / 1000);
-    fetch(`https://finnhub.io/api/v1/stock/candle?symbol=${stock}&resolution=D&from=${previousMonth}&to=${currDay}&token=${apiKey}`)
-    .then(response => response.json())
-    .then(candleData => graphData(candleData))
+    fetch(`https://finnhub.io/api/v1/quote?symbol=${stock}&token=${apiKey}`)
+      .then((response) => response.json())
+      .then((stockData) => writeData(stockData, stock))
+      .then(getCandleData(stock));
+  });
 }
-function writeData(stockData, stock){
-    //gets rid of uneccessary and previous data
-    //console.log(stockData.t)
-    let table = document.getElementById('stockInfo');
-    if(table.rows.length > 1){table.deleteRow(1)}
-    //create row and add name and stock data 
-    let priceRow = document.createElement('tr');
-    let stockName = document.createElement('td')
-    stockName.innerText = stock;
-    priceRow.appendChild(stockName)
-    for(var value in stockData){
-        let priceData = document.createElement('td')
-        priceData.innerText = stockData[value];
-        priceRow.appendChild(priceData);
-    }
-    stockInfo.appendChild(priceRow);
+
+function getCandleData(stock) {
+  let previousMonth = Math.round(
+    new Date(new Date().setDate(new Date().getDate() - 30)) / 1000
+  );
+  let currDay = Math.round(new Date().getTime() / 1000);
+  fetch(
+    `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=${alphaApi}`
+  )
+    .then((response) => response.json())
+    .then((candleData) => graphData(candleData));
+}
+function writeData(stockData, stock) {
+  //gets rid of uneccessary and previous data
+  delete stockData.t;
+  let table = document.getElementById("stockInfo");
+  if (table.rows.length > 1) {
+    table.deleteRow(1);
+  }
+  //create row and add name and stock data
+  let priceRow = document.createElement("tr");
+  let stockName = document.createElement("td");
+  stockName.innerText = stock;
+  priceRow.appendChild(stockName);
+  for (var value in stockData) {
+    let priceData = document.createElement("td");
+    priceData.innerText = stockData[value];
+    priceRow.appendChild(priceData);
+  }
+  stockInfo.appendChild(priceRow);
 }
 
 getCurrentPrice();
-function graphData(){
-    const ctx = document.getElementById('myChart');
+function graphData(closeData) {
+  //grabs last 30 days and the closing prices from JSON obj
+  let prevDate = [];
+  let prevHigh = [];
+  let prevLow = [];
+  let prevClose = [];
+  
+  let keys = Object.keys(closeData["Time Series (Daily)"]);
+  for (var i = 0; i < 30; i++) {
+    let day = keys[i];
+    let high = closeData["Time Series (Daily)"][keys[i]]["2. high"];
+    let low = closeData["Time Series (Daily)"][keys[i]]["3. low"];
+    let close = closeData["Time Series (Daily)"][keys[i]]["4. close"];
+    prevDate.push(day);
+    prevClose.push(close);
+    prevLow.push([low, close]);
+    prevHigh.push([close, high]);
+   // prevClose.push(close);
+  }
+  chartData(prevDate, prevClose, prevHigh, prevLow);
+}
 
-    const myChart = new Chart(ctx, {
-        type: 'bar',
+function chartData(prevDate, prevClose, prevHigh, prevLow){
+    const ctx = document.getElementById('myChart');
+    console.log(prevClose)
+    new Chart(ctx, {
+        
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+          labels: prevDate,
+          datasets: [
+            { 
+                type: 'bar',
+                label: 'Previous Low',
+                data: prevLow,
+                backgroundColor: "rgba(240, 0, 0, 1.0)"
+            },
+            {
+                type: 'bar',
+                label: 'Previous High',
+                data: prevHigh,
+                backgroundColor: "rgba(0, 200, 0, 0.8)"
+            },
+            {
+                type:'line',
+                label: 'Closing Value',
+                data: prevClose,
+                borderColor: 'rgba(255, 255, 255, 1)',
+                backgroundColor: 'rgba(255, 255, 255, 1)'
+            },
+          ]
         },
         options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
+            plugins:{
+                title: {
+                    display: true,
+                    text: '30-Day Stock Trend',
+                    color: 'rgba(255, 255, 255, 1)'
+                  }
+            },
+            responsive: false,
+            //maintainAspectRatio: false
         }
-    });
+      });
 }
+
+new Chart(document.getElementById("bar-chart-horizontal"), {
+    type: 'horizontalBar',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
